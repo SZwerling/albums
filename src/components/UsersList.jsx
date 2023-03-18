cd3458fd785c910eda8eef1bc834a198c47f5940
unknown <sazwerling@gmail.com>
2023-03-18 14:04:18 -0700

    put loading state and error in component state

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { render } from "react-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
@@ -7,23 +7,32 @@ import Skeleton  from "./Skeleton"
 import Button from "./Button";
 
 function UsersList() {
+
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false)
+    const [loadingUsersError, setLoadingUsersError] = useState(null)
+
     const dispatch = useDispatch()
-    const {isLoading, data, error} = useSelector((state) => {
+
+    const { data } = useSelector((state) => {
         return state.users
     })
 
     useEffect(() => {
+        setIsLoadingUsers(true)
         dispatch(fetchUsers())
+        .unwrap()
+        .catch((err) => setLoadingUsersError(err))
+        .finally(() => setIsLoadingUsers(false)) 
     }, [])
 
     const handleUserAdd = () => {
         dispatch(addUser())
     }
 
-    if(isLoading){
+    if(isLoadingUsers){
         return <Skeleton times={6} size={"h-10 w-full"}/>
     }
-    if(error){
+    if(loadingUsersError){
         return <div>Error fetching data..</div>
     }
 
