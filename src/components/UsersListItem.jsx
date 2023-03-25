0c8de7cba9e62fb08672d8aa6c11e883ad279bc7
unknown <sazwerling@gmail.com>
2023-03-24 20:13:04 -0700

    styling for delete button and user list item

--- src/components/UsersListItem.jsx
+++ src/components/UsersListItem.jsx
@@ -3,22 +3,25 @@ import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
 
-function UsersListItem ({ user }) {
-    const [doRemoveUser, isLoading, error] = useThunk(removeUser)
-    const handleClick = () => {
-        doRemoveUser(user)
-    }
+function UsersListItem({ user }) {
+   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+   const handleClick = () => {
+      doRemoveUser(user);
+   };
 
-    return (
-             <div className="mb-2 border rounded">
-               <div className="flex p-2 justify-between items-center cursor-pointer">
-                <Button loading={isLoading} onClick={handleClick}><GoTrashcan/></Button>
-                {error && <div>error deleting user</div>}
-                  {user.name}
-
-               </div>
+   return (
+      <div className="mb-2 border rounded">
+         <div className="flex p-2 justify-between items-center cursor-pointer">
+            <div className="flex flex-row items-center justify-between">
+               <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+                  <GoTrashcan />
+               </Button>
+               {error && <div>error deleting user</div>}
+               {user.name}
             </div>
-         );
+         </div>
+      </div>
+   );
 }
 
 export default UsersListItem;
