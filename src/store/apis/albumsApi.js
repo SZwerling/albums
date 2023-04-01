e8bd10e068da8ac14a7f6ff122315c0a4f9cd266
unknown <sazwerling@gmail.com>
2023-04-01 15:39:56 -0700

    connect albumsApi to the store

---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const ablumsApi = createApi({
+    reducerPath: 'albums',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005'
+    }),
+    endpoints(builder) {
+        return {
+            fetchAlbums: builder.query({   //now we get access to a hook called albumsApi.useFetchAlbumsQuery()
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        params: {
+                            userId: user.id
+                        },
+                        method: 'GET',
+                    }
+                }
+            })
+        }
+    }
+})
+
+export const { useFetchAlbumsQuery } = albumsApi;
+export { albumsApi }
