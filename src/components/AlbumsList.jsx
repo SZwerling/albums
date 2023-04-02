53a523d082973e05b10a652040390281e48416d0
unknown <sazwerling@gmail.com>
2023-04-01 21:38:37 -0700

    render album list w expandable panel

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,12 +1,30 @@
 import { useFetchAlbumsQuery } from "../store/apis/albumsApi";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList({ user }) {
     const {data, error, isLoading} = useFetchAlbumsQuery(user);
-
-    console.log(data, error, isLoading)
+    let content;
+    if(isLoading){
+        content = <Skeleton times={3}/>
+    } else if(error){
+        content = <div>Error Loading Albums</div>
+    } else {
+        content = data.map((album => {
+            const header = <div>{album.title}</div>
+            return <ExpandablePanel header={header} key={album.id}>
+                List of Photos in the Album
+            </ExpandablePanel>
+        })) 
+    }
 
     return (
-        <div>Albums for {user.name}</div>
+        <div>
+            <div>
+                {content}
+            </div>
+        </div>
     )
 }
 
